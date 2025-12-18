68513329565a6abafa185876f398996abb97489a
0xahzam <ahzam.main@gmail.com>
2025-12-18 16:07:17 +0530

    convert to debug log

--- src/liquidator.rs
+++ src/liquidator.rs
@@ -1323,7 +1323,7 @@ fn spawn_liquidation_worker(
                 .get(&liquidatee)
                 .is_some_and(|last| slot.abs_diff(*last) < LIQUIDATION_SLOT_RATE_LIMIT)
             {
-                log::info!(target: TARGET, "rate limited liquidation for {:?} (current: {})", liquidatee, slot);
+                log::debug!(target: TARGET, "rate limited liquidation for {:?} (current: {})", liquidatee, slot);
                 continue;
             } else {
                 rate_limit.insert(liquidatee, slot);
